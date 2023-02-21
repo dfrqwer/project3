e7915376ed450f9f0b18753fb15164a51f8f16d2
soundwave <93002066+ireone01@users.noreply.github.com>
2023-02-22 01:34:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,23 +36,23 @@ search.addEventListener('click', () => {
 
             switch (json.weather[0].main) {
                 case 'Clear':
-                    image.src = 'clear.png';
+                    image.src = 'images/clear.png';
                     break;
 
                 case 'Rain':
-                    image.src = 'rain.png';
+                    image.src = 'images/rain.png';
                     break;
 
                 case 'Snow':
-                    image.src = 'snow.png';
+                    image.src = 'images/snow.png';
                     break;
 
                 case 'Clouds':
-                    image.src = 'cloud.png';
+                    image.src = 'images/cloud.png';
                     break;
 
                 case 'Haze':
-                    image.src = 'mist.png';
+                    image.src = 'images/mist.png';
                     break;
 
                 default:
